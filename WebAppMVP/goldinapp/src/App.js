2270ae938de15f285a54a358318be71a4aae335c
SmileCoder1 <venterap@gmail.com>
2024-01-26 01:02:57 -0600

    added app

---
+++ WebAppMVP/goldinapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="a">
+        <p>hello</p>
+      </div>
+      <div className="b">
+        <p>hello2</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
